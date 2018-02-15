53c860a8725d5954fe9fd675f4b959e155e370ff
Oriana Ruiz <oriru92@gmail.com>
2018-02-14 23:00:24 -0400

    modificando acceso de assets.

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -29,20 +29,20 @@
         <link href="http://fonts.googleapis.com/css?family=Raleway:300,500,900%7COpen+Sans:400,700,400italic" rel="stylesheet" type="text/css" />
 
         <!-- Bootstrap CSS -->
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+        <link href="{{ secure_asset('css/bootstrap.min.css') }}" rel="stylesheet" />
 
         <!-- Template CSS -->
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+        <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet" />
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" />
 
         <!-- Modernizr -->
-        <script src="{{ asset('js/modernizr-2.8.1.min.js') }}"></script>
+        <script src="{{ secure_asset('js/modernizr-2.8.1.min.js') }}"></script>
 
          <script>
             // rename myToken as you like
             window.myToken =  <?php echo json_encode([
-                'csrfToken' => csrf_token(),
-            ]); ?>
+		'csrfToken' => csrf_token(),
+	]);?>
         </script>
 
     </head>
